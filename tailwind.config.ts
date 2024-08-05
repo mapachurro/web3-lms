f53a5e62fca88746fd479f9a4b21b1af72f30ee4
“shrutipanjwani” <hello@shrutipanjwani.com>
2024-08-05 13:45:47 +0530

    added fonts and updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,34 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        "max-350": { max: "350px" },
+      },
+      fontFamily: {
+        polysans: ["PolySans Neutral"],
+        cg: ["CabinetGrotesk Variable"],
+        "cg-regular": ["CabinetGrotesk Regular"],
+        "cg-light": ["CabinetGrotesk Light"],
+        "cg-thin": ["CabinetGrotesk Thin"],
+        "cg-medium": ["CabinetGrotesk Medium"],
+        "cg-black": ["CabinetGrotesk Black"],
+        "cg-bold": ["CabinetGrotesk Bold"],
+        "cg-extrabold": ["CabinetGrotesk ExtraBold"],
+      },
+      colors: {
+        black: "#0e1016",
+        white: "#fff",
+        grey: "#1a1a1a",
+        primary: "#0055FF",
+        secondary: "#90B5FF",
       },
     },
   },
