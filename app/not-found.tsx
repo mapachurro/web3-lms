39ddd0659cb4b4ebe02fb73bea023295da75f18f
“shrutipanjwani” <hello@shrutipanjwani.com>
2024-08-06 15:34:41 +0530

    added a not found component

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h1 className="text-white text-center mt-10 text-xl font-polysans">
+          Something went wrong
+        </h1>
+      </body>
+    </html>
+  );
+}
