e17e829ff300b425258319a9f3a6e72a24540158
“shrutipanjwani” <hello@shrutipanjwani.com>
2024-08-07 20:17:59 +0530

    module footer and fixes

--- app/[locale]/modules/page.tsx
+++ app/[locale]/modules/page.tsx
@@ -3,9 +3,17 @@
 import React from "react";
 import withAuth from "@/utils/withAuth";
 import ModulesPage from "@/components/Modules";
+import ModulesNavbar from "@/components/ModulesNavbar";
+import ModulesFooter from "@/components/ModulesFooter";
 
 const Modules = () => {
-  return <ModulesPage />;
+  return (
+    <div className="relative isolate bg-no-repeat bg-cover bg-center bg-[url('/images/modules-bg.svg')] overflow-hidden">
+      <ModulesNavbar />
+      <ModulesPage />
+      <ModulesFooter />
+    </div>
+  );
 };
 
 export default withAuth(Modules);
