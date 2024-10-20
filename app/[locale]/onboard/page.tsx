af1866014c5c16380b5bad56db4548ce693b7dfa
“shrutipanjwani” <hello@shrutipanjwani.com>
2024-10-21 04:18:06 +0530

    added navbar in onboarding

--- app/[locale]/onboard/page.tsx
+++ app/[locale]/onboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ModulesFooter from "@/components/ModulesFooter";
+import ModulesNavbar from "@/components/ModulesNavbar";
 import Onboarding from "@/components/Onboarding";
 import withAuth from "@/utils/withAuth";
 import React from "react";
@@ -7,7 +9,9 @@ import React from "react";
 const Onboard = () => {
   return (
     <div className="relative isolate bg-no-repeat bg-cover bg-center bg-[url('/images/modules-bg.svg')] overflow-hidden">
+      <ModulesNavbar />
       <Onboarding />
+      <ModulesFooter />
     </div>
   );
 };
